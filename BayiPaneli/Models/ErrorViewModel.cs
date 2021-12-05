4abe879a2518afb57f2ef8f99b5cc338555b4998
Mehmet Kanyılmaz <mehmetkanyilmaz@outlook.com.tr>
2021-12-05 21:25:55 +0300

    Add project files.

---
+++ BayiPaneli/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BayiPaneli.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
